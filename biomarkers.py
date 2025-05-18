57eab2275f022b24092b3e0e02132388f9bd20ab
Collin Andrews <pizzaTrucks@outlook.com>
2025-05-18 14:04:33 -0500

    Add files via upload

---
+++ biomarkers.py
@@ -0,0 +1,7 @@
+from functions import BrainData
+
+
+inPathFolder = '/Users/ca34522/Documents/Bioinformatics/SEA-AD/'
+inLoadFile = 'sea-ad_all_mtg_quant_neuropath_bydonorid_081122.csv'
+
+brains = BrainData(pathFolder=inPathFolder, fileName=inLoadFile)
